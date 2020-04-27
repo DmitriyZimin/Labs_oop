6ae07f15c56fed27c8871f28194f770295bd47c6
Dmitriy <demaxru@gmail.com>
2020-04-27 21:12:39 +0300

    Lab_5_Attributes,Exceptions

---
+++ Lab_3/Lab_3/MyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3
+{
+    class MyAttribute : Attribute
+    {
+        public string author { get; set; }
+    }
+}
--- Lab_3/Lab_3/Program.cs
+++ Lab_3/Lab_3/Program.cs
@@ -6,17 +6,18 @@ namespace Lab_3
 {
     class Program
     {
+        [My(author = "Zimin D.")]
         static void Main(string[] args)
         {
             List<Passenger_cars> d = new List<Passenger_cars>();
-
+           
             Passenger_cars x1 = new Passenger_cars("Toyota", 106, 1549000.20);
             Passenger_cars x2 = new Passenger_cars("Lada", 87, 549000.20);
             Passenger_cars x3 = new Passenger_cars("Skoda", 122, 1249000.20);
 
             x1.GetInfo();
-            x2.GetInfo();
-            x3.GetInfo();
+            //x2.GetInfo();
+            //x3.GetInfo();
             x1.Move();
             x1.TrasportingPassengers();
 
@@ -24,29 +25,66 @@ namespace Lab_3
             d.Add(x2);
             d.Add(x3);
 
-            int x = d.Where(e => e.price > 1000000.00).Count();
+            //int x = d.Where(e => e.price > 1000000.00).Count();
 
-            Console.WriteLine(x + " cars with price > 1000000.00");
+            //Console.WriteLine(x + " cars with price > 1000000.00");
 
-            Buses y = new Buses();
+            {
+                while (true)
+                {
+                    double a, b;
+                    
+                    try
+                    {
+                        Console.WriteLine("Spent fuel (litres)");
+                        a = double.Parse(Console.ReadLine());
 
-            y.name = "Man";
-            y.horsepower = 120;
-            y.price = 1870000.80;
+                        Console.WriteLine("Kilometrage");
+                        b = double.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Error! Wrong format!");
+                        continue;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Error!");
+                        continue;
+                    }
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Error! Division by zero!");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(a / b * 100 + " litres for 100 km");
+                    }
+                    break;
+                }
 
-            y.GetInfo();
-            y.Move();
-            y.TrasportingPassengers();
 
-            Trucks z = new Trucks();
+                //Buses y = new Buses();
 
-            z.name = "Kamaz";
-            z.horsepower = 150;
-            z.price = 2000000.30;
+                //y.name = "Man";
+                //y.horsepower = 120;
+                //y.price = 1870000.80;
 
-            z.GetInfo();
-            z.Move();
-            z.CargoDelivery();
+                //y.GetInfo();
+                //y.Move();
+                //y.TrasportingPassengers();
+
+                //Trucks z = new Trucks();
+
+                //z.name = "Kamaz";
+                //z.horsepower = 150;
+                //z.price = 2000000.30;
+
+                //z.GetInfo();
+                //z.Move();
+                //z.CargoDelivery();
+            }
         }
     }
 }
